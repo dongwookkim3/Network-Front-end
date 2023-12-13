699324f4ca24d4d20d58ac9f195f08b14be9fce3
dongwookkim3 <dongwook.3.kim@gmail.com>
2023-12-13 10:21:52 +0900

    DOCS :: 전체코드 최적화

--- MainPlantPage/script.js
+++ MainPlantPage/script.js
@@ -15,39 +15,37 @@ const database = firebase.database();
 
 const ref1 = database.ref("Smart_Plant/DHT1");
 const ref2 = database.ref("Smart_Plant/DHT2");
-const ref3 = database.ref("Smart_Plant/GYRO");
-const ref4 = database.ref("Smart_Plant/PHOTO/CAM1");
-const ref5 = database.ref("Smart_Plant/PLANTS");
-const ref6 = database.ref("Smart_Plant");
-const ref7 = database.ref("Smart_Plant/PLANTS");
-const ref8 = database.ref("Smart_Plant/PHOTO/CAM2");
-const ref9 = database.ref("Smart_Plant/SOILMOISTURE");
+const ref3 = database.ref("Smart_Plant/SOILMOISTURE");
+const ref4 = database.ref("Smart_Plant");
+const ref5 = database.ref("Smart_Plant/PHOTO/CAM1");
+const ref6 = database.ref("Smart_Plant/PHOTO/CAM2");
+
 
 ref1.once("value").then((snapshot1) => {
   const data1 = snapshot1.val();
   return ref2.once("value").then((snapshot2) => {
     const data2 = snapshot2.val();
-    return ref9.once("value").then((snapshot9) => {
-      const data9 = snapshot9.val();
-      const soilMoisture = (100 - (data9.SoilMoisture / 1024) * 100).toFixed(2);
+    return ref3.once("value").then((snapshot3) => {
+      const data3 = snapshot3.val();
+      const soilMoisture = (100 - (data3.SoilMoisture / 1024) * 100).toFixed(2);
       const humidity = ((data1.humidity + data2.humidity) / 2).toFixed(2);
       const temperature = ((data1.temperature + data2.temperature) / 2).toFixed(2);
       document.querySelector("#container1").innerHTML = temperature + "°C";
       document.querySelector("#container2").innerHTML = humidity + "%";
       document.querySelector("#container3").innerHTML = soilMoisture + "%";
-      
+
       suggestsoilMoisture(soilMoisture)
       suggestTemperature(temperature);
       suggestHumidity(humidity);
     });
   });
 })
-  .catch ((error) => {
-  console.error(error);
-});
+  .catch((error) => {
+    console.error(error);
+  });
 
-function suggestsoilMoisture(soilMoisture){
-  if (soilMoisture<=31.64) {
+function suggestsoilMoisture(soilMoisture) {
+  if (soilMoisture <= 31.64) {
     document.querySelector('#soi').innerHTML = '물이 너무 부족해요.'
   }
 }
@@ -72,23 +70,23 @@ function suggestHumidity(humidity) {
 }
 
 
-ref4.once("value").then((snapshot4) => {
-  const data4 = snapshot4.val();
-  var photoURL = data4.ESP32CAM1;
+ref5.once("value").then((snapshot5) => {
+  const data5 = snapshot5.val();
+  var photoURL = data5.ESP32CAM1;
   var img = document.getElementById("img1");
-  return (img.src = photoURL);
-});
-
-ref8.once("value").then((snapshot8) => {
-  const data8 = snapshot8.val();
-  var photoURL = data8.ESP32CAM2;
-  var img = document.getElementById("img2");
   return (img.src = photoURL);
 });
 
 ref6.once("value").then((snapshot6) => {
   const data6 = snapshot6.val();
-  if (data6.unzi === 1) {
+  var photoURL = data6.ESP32CAM2;
+  var img = document.getElementById("img2");
+  return (img.src = photoURL);
+});
+
+ref4.once("value").then((snapshot4) => {
+  const data4 = snapshot4.val();
+  if (data4.unzi === 1) {
     return (document.querySelector("#unzi").innerHTML = "식물이 넘어졌습니다!");
   }
 });
@@ -117,11 +115,7 @@ const getBoards = async () => {
 
 function displayBoards(board) {
   const table = document.getElementById("board-table");
-
-  if (!table) {
-    return;
-  }
-
+  if (!table) return;
   table.innerHTML = "";
 
   const headerRow = document.createElement("tr");
--- Select/script.js
+++ Select/script.js
@@ -3,18 +3,15 @@ function updateDropdown() {
   var subCategorySelect = document.getElementById("sub-category");
   var selectedCategory = categorySelect.value;
 
-  // Hide and disable sub-category dropdown if no category is selected
   if (selectedCategory === "") {
     subCategorySelect.style.display = "none";
     subCategorySelect.disabled = true;
     return;
   }
 
-  // Clear previous options
   subCategorySelect.innerHTML = '<option value="">선택</option>';
   subCategorySelect.disabled = false;
 
-  // Add new options based on the selected category
   if (selectedCategory === "plant") {
     addOption(subCategorySelect, "장미");
     addOption(subCategorySelect, "진달래");
@@ -25,7 +22,6 @@ function updateDropdown() {
     addOption(subCategorySelect, "향나무");
   }
 
-  // Show and enable sub-category dropdown
   subCategorySelect.style.display = "inline-block";
   subCategorySelect.disabled = false;
 }
@@ -37,7 +33,6 @@ function addOption(select, value) {
   select.add(option);
 }
 
-// Firebase 구성 정보
 const firebaseConfig = {
   apiKey: "AIzaSyBdvMg1__shb51NA8BlG4Mn8IdNz_HK8E0",
   authDomain: "smartplant-89fe3.firebaseapp.com",
@@ -49,22 +44,17 @@ const firebaseConfig = {
   measurementId: "G-B42952QHB3",
 };
 
-// Firebase 초기화
 firebase.initializeApp(firebaseConfig);
 
-// Firebase 데이터베이스 참조
 var database = firebase.database();
 
-// 데이터 추가 함수
 function addData() {
   var plantName = document.getElementById("plantname").value;
   var category = document.getElementById("category").value;
   var subCategory = document.getElementById("sub-category").value;
 
-  // 새로운 데이터 키 생성
   var newKey = database.ref("Smart_Plant/PLANTS/").child("PLANTS").push().key;
 
-  // 데이터 추가
   var updates = {};
   updates["Smart_Plant/PLANTS/" + newKey] = {
     name: plantName,
@@ -76,16 +66,13 @@ function addData() {
     .ref()
     .update(updates)
     .then(function () {
-      // 데이터 추가 후 입력 필드 초기화
       document.getElementById("plantname").value = "";
       document.getElementById("category").value = "";
       document.getElementById("sub-category").value = "";
 
-      // 데이터 전송 성공 알림
       alert("데이터가 성공적으로 전송되었습니다.");
     })
     .catch(function (error) {
-      // 데이터 전송 실패 알림
       alert("데이터 전송에 실패했습니다. 다시 시도해주세요.");
       console.error(error);
     });
